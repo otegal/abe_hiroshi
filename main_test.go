0dcb3a87289f51d424b4ffbf7ae52e38bcbc10b2
hhiroki <h-hiroki@users.noreply.github.com>
2019-04-03 21:12:00 +0900

    ⛳️ディレクトリ名更新

--- main_test.go
+++ main_test.go
@@ -5,9 +5,9 @@ import (
 )
 
 func TestMain(t *testing.T) {
-	// ローカルに保存したHTMLをMockとしてテストする
+	// ローカルに保存したHTMLに向けてテストする
 	// テスト用に値を変更
-	pageURL = "file:///Users/h_hiroki/go/src/abe_hiroshi/MockHtml/阿部寛のホームページ.html"
+	pageURL = "file:///Users/h_hiroki/go/src/abe_hiroshi/DammyHtml/阿部寛のホームページ.html"
 	screenShotFileName = "test_screen_shot.jpg"
 
 	// 実際の処理を実行
